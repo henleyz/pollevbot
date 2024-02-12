f5642cfac694cb4ee0d492b36e743e0c97718e96
Henley <henley@berkeley.edu>
2024-02-12 15:35:45 -0800

    update readme + locked polls

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pollevbot
- 
+# Pollevbot
+a simple bot for auto responding to pollev questions in a class
+
+## Usage
+
+
+1. Install selenium
+
+```pip install selenium```
+2. Install the chrome driver
+Download chrome driver from [here](https://sites.google.com/chromium.org/driver/)
+Set the path of the chrome driver in chrome_driver_path variable in config.json
+
+3. Get the user data path so that you stay logged in to pollev
+
+In google chrome, open a new tab and type chrome://version
+Copy the profile path and set it in the user_data_path variable in config.json
+
+4. Set the presenter code in config.json or pass it as an argument to the main function
+
+5. Run the bot
+
+```python main.py [optional presenter code]```
--- main.py
+++ main.py
@@ -6,18 +6,37 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import re
-PRESENTER_CODE = "henleyzhang720"
+import json
+import sys
+
+
+
+
+PRESENTER_CODE = "aried"
+CHROME_DRIVER_PATH = "C:\\Users\\henle\\Desktop\\chromedriver.exe"
+CHROME_PROFILE_PATH = "C:\\Users\\henle\\AppData\\Local\\Google\\Chrome\\User Data"
+
+#get vars from config.json
+with open('config.json') as f:
+    data = json.load(f)
+    PRESENTER_CODE = data['presenter_code']
+    CHROME_DRIVER_PATH = data['chrome_driver_path']
+    CHROME_PROFILE_PATH = data['chrome_profile_path']
+
+
+if len(sys.argv) > 1:
+    PRESENTER_CODE = sys.argv[1]
 
 def main():
         #TODO change where your chrome webdriver is
-        service = Service("C:\\Users\\henle\\Desktop\\chromedriver.exe")
+        service = Service(CHROME_DRIVER_PATH)
 
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
 
         #TODO Replace "henle" with your user folder name
-        options.add_argument("user-data-dir=C:\\Users\\henle\\AppData\\Local\\Google\\Chrome\\User Data")
+        options.add_argument("user-data-dir=" + CHROME_PROFILE_PATH)
 
         options.add_argument('--profile-directory=Default')
 
@@ -36,6 +55,15 @@ def main():
                 number_of_buttons_to_select = 1
                 poll_header = driver.find_element(By.XPATH, '//div[@class="component-response-header__status"]')
                 header_text = poll_header.text
+
+                # check if poll is locked
+                is_locked = "locked" in header_text
+                if is_locked:
+                    print("Poll is locked, waiting for next poll")
+                    time.sleep(1)
+                    continue
+                
+                # check if poll is a select all that apply poll, and select a random number of buttons
                 match = re.search(r'\d+', header_text)
                 if match:
                     print("Responding to select all that apply poll with " + match.group() + " options")
@@ -49,8 +77,9 @@ def main():
                 for button in selected_buttons:
                     button.click()
                     time.sleep(0.5)
-                time.sleep(60)
+                time.sleep(30)
             except:
+                #if poll is not found, wait for 1 second and check again
                 time.sleep(1)
                 print("Checking for polls")
 
